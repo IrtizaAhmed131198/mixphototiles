5ed13331c78817ecb028311b742635708965952f
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-04-11 21:51:11 +0500

    asas

--- resources/views/livewire/design-page.blade.php
+++ resources/views/livewire/design-page.blade.php
@@ -221,53 +221,55 @@
                                         </p>
                                     </div>
 
-                                    <?php foreach ($custom_color as $key => $val): ?>
-                                    <?php
+                                    @foreach ($custom_color as $key => $val)
+                                    @php
                                         $cssClassName = strtolower(str_replace(' ', '-', $val->name));
-                                        echo '<style>
-                                            .box-shadow-' . $cssClassName . '::before {
-                                                position: absolute;
-                                                z-index: -1;
-                                                content: "";
-                                                right: -8px;
-                                                top: 4px;
-                                                bottom: 0;
-                                                height: 100%;
-                                                width: 8px;
-                                                background: ' . $val->before_color_code . ';
-                                                transform: skewY(45deg);
-                                            }
+                                    @endphp
 
-                                            .box-shadow-' . $cssClassName . '::after {
-                                                position: absolute;
-                                                z-index: -1;
-                                                content: "";
-                                                background: ' . $val->after_color_code . ';
-                                                width: 100%;
-                                                height: 8px;
-                                                bottom: -8px;
-                                                transform: skewX(45deg);
-                                                left: 5px;
-                                            }
-                                        </style>';
-                                    ?>
+                                    <style>
+                                        .box-shadow-{{ $cssClassName }}::before {
+                                            position: absolute;
+                                            z-index: -1;
+                                            content: "";
+                                            right: -8px;
+                                            top: 4px;
+                                            bottom: 0;
+                                            height: 100%;
+                                            width: 8px;
+                                            background: {{ $val->before_color_code }};
+                                            transform: skewY(45deg);
+                                        }
+
+                                        .box-shadow-{{ $cssClassName }}::after {
+                                            position: absolute;
+                                            z-index: -1;
+                                            content: "";
+                                            background: {{ $val->after_color_code }};
+                                            width: 100%;
+                                            height: 8px;
+                                            bottom: -8px;
+                                            transform: skewX(45deg);
+                                            left: 5px;
+                                        }
+                                    </style>
 
                                     <li type="button"
-                                        class="parentProperties dropdown-item frame-color <?php echo ($key == 0 ? 'li-border-color' : ''); ?>"
-                                        data-price="<?php echo $val->price; ?>"
-                                        data-color="<?php echo $val->name; ?>"
-                                        data-src="<?php echo $val->frame_img; ?>"
-                                        data-shadow="box-shadow-<?php echo $cssClassName; ?>">
+                                        class="parentProperties dropdown-item frame-color {{ $key == 0 ? 'li-border-color' : '' }}"
+                                        data-price="{{ $val->price }}"
+                                        data-color="{{ $val->name }}"
+                                        data-src="{{ $val->frame_img }}"
+                                        data-shadow="box-shadow-{{ $cssClassName }}">
                                         <figure class="PropertiesleftChild">
                                             <img alt="drawer" width="72" height="72" class="LeftSidebar"
-                                                 src="<?php echo asset($val->option_img); ?>">
+                                                src="{{ asset($val->option_img) }}">
                                         </figure>
                                         <div class="PropertiesRightChild">
-                                            <p class="propertyName"><?php echo $val->name; ?></p>
-                                            <p class="propertyPrize">$<?php echo $val->price; ?></p>
+                                            <p class="propertyName">{{ $val->name }}</p>
+                                            <p class="propertyPrize">${{ $val->price }}</p>
                                         </div>
                                     </li>
-                                <?php endforeach; ?>
+                                @endforeach
+
 
 
                                     {{-- <li type="button" class="parentProperties dropdown-item frame-color li-border-color" data-price="0"
