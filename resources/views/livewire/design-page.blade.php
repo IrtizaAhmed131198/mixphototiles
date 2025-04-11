eab1aaef8d2d38a91b63e1a9266cd84c6119f68d
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-04-11 21:37:09 +0500

    a

--- resources/views/livewire/design-page.blade.php
+++ resources/views/livewire/design-page.blade.php
@@ -221,6 +221,7 @@
                                         </p>
                                     </div>
 
+                                    <?php $cssClassName = ''; ?>
                                     @foreach ($custom_color as $key => $val)
                                         <?php
                                             $cssClassName = strtolower(str_replace(' ', '-', $val->name));
